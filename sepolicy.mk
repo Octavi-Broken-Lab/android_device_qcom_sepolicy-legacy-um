b23b3add30b5aca02e42260c4a6802b706b5b2d2
Sagarrokade006 <sagarrokade006@gmail.com>
2020-07-07 06:05:48 +0000

    Init Octavi

--- sepolicy.mk
+++ sepolicy.mk
@@ -27,4 +27,4 @@ BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/qcom/sepolicy-legacy-um/private
 
--include device/lucid/sepolicy/qcom/sepolicy.mk
+-include device/octavi/sepolicy/qcom/sepolicy.mk
